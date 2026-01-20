f1d66d61c85c32a284c7ff2b2e92691e4d7c840c
Pavel Gorshkov <gorpavel117@yandex.ru>
2026-01-21 00:02:36 +0300

    task_1

---
+++ task_1.py
@@ -0,0 +1,18 @@
+string_origin = '1h 45m,360s,25m,30m 120s,2h 60s'
+
+string_list = string_origin.replace(' ', ',').split(',')
+
+print(string_list)
+
+summ_minutes = 0
+
+for i in string_list:
+    if('h' in i):
+        summ_minutes += 60 * int(i[:len(i) - 1])
+    elif('m' in i):
+        summ_minutes += int(i[:len(i) - 1])
+    else:
+        summ_minutes += (1 / 60) * int(i[:len(i) - 1])
+
+print('Общее количество минут:', summ_minutes)
+    
